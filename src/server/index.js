6f6e11a26e7c98ef8e33d49078ceb8a00c689ef5
AbdelrahmanElmughrabi <155820942+AbdelrahmanElmughrabi@users.noreply.github.com>
2025-07-07 14:51:41 +0300

    deployment fix 3

--- src/server/index.js
+++ src/server/index.js
@@ -107,6 +107,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
